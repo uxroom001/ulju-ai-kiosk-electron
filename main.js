c88e6223672c15d981a6823d286960a7c2bd4bde
careal98 <careal98@gmail.com>
2026-03-23 15:45:28 +0900

    feat: ...

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 const { app, BrowserWindow } = require("electron");
+const { autoUpdater } = require("electron-updater");
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1080,
         height: 1920,
         fullscreen: true,
-        autoHideMenuBar: true,
         kiosk: true,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: false,
+        },
     });
 
     win.loadURL("https://ulju-ai-kiosk.vercel.app/");
+
+    // 뒤로가기 차단
+    win.webContents.on("before-input-event", (event, input) => {
+        if (input.key === "BrowserBack") {
+            event.preventDefault();
+        }
+    });
+
+    // 개발자도구 막기
+    win.webContents.on("devtools-opened", () => {
+        win.webContents.closeDevTools();
+    });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+
+    autoUpdater.checkForUpdatesAndNotify();
+});
